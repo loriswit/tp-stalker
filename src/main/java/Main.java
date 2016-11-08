516243859e489d63fe9f8898e38a84dce2dae2cb
Loris Witschard <loris.wit@gmail.com>
2016-11-08 16:33:27 +0100

    Catch exceptions in main function

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,27 @@
-import javax.swing.UIManager;
+import javax.swing.*;
 import java.util.Timer;
 
 public class Main
 {
     public static void main(String[] args) throws Exception
     {
-        UIManager.setLookAndFeel(
-            UIManager.getSystemLookAndFeelClassName());
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         
-        MainWindow window = new MainWindow();
-    
         Timer timer = new Timer();
-        timer.schedule(window, 5000, 5000);
+        
+        try
+        {
+            MainWindow window = new MainWindow();
+            timer.schedule(window, 5000, 5000);
+        }
+        catch(Exception e)
+        {
+            JOptionPane.showMessageDialog(null, "Error: " + e.toString() + "\nCheck your Internet connexion.",
+                "Error", JOptionPane.ERROR_MESSAGE);
+            
+            e.printStackTrace();
+            
+            timer.cancel();
+        }
     }
 }
